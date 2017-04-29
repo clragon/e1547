b9a81103e5fca94b9abd403d0a1eb6fea6fbb572
perlatus <perlatus@git.email.vczf.io>
2017-04-29 05:06:32 -0400

    Load a small page of posts and display their IDs
    Open questions:
    - How do I load more posts once I hit the end of the grid as well as at
      init?
    - How do I load images without making a mess in build?

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,36 @@ void main() {
   runApp(new E1547App());
 }
 
-class E1547App extends StatelessWidget {
+class E1547App extends StatefulWidget {
+  @override
+  _E1547AppState createState() => new _E1547AppState();
+}
+
+class _E1547AppState extends State<E1547App> {
+  HttpClient _http = new HttpClient();
+
+  List<Map> _posts = [];
+
+  Future _loadPosts() async {
+    if (!_posts.isEmpty) {
+      return;
+    }
+
+    HttpClientResponse response = await _http.getUrl(
+        Uri.parse("https://e621.net/post/index.json?page=1&limit=20"))
+      .then((HttpClientRequest req) => req.close());
+
+    var body = new StringBuffer();
+    await response.transform(UTF8.decoder)
+      .forEach((s) => body.write(s));
+
+    var posts = JSON.decode(body.toString());
+    setState(() => this._posts = posts);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _loadPosts();
     return new MaterialApp(
       title: 'E1547',
       theme: new ThemeData.dark(),
@@ -21,7 +48,12 @@ class E1547App extends StatelessWidget {
           gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200.0, // px
           ),
-          itemBuilder: (ctx, i) => new Center(child: new Text(i.toString())),
+          itemBuilder: (ctx, i) {
+            return new Center(child: _posts.length > i ?
+                new Text(_posts[i]['id'].toString()) :
+                const Text("")
+            );
+          },
         ),
       ),
     );
