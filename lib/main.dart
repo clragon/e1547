6f462492368ae0f45df8ef1d524c099199ed9bb6
perlatus <perlatus@git.email.vczf.io>
2017-04-29 04:04:55 -0400

    Make a stub grid for posts

--- lib/main.dart
+++ lib/main.dart
@@ -6,109 +6,24 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new E1547App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class E1547App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting
-        // the app, try changing the primarySwatch below to Colors.green
-        // and then invoke "hot reload" (press "r" in the console where
-        // you ran "flutter run", or press Run > Hot Reload App in IntelliJ).
-        // Notice that the counter didn't reset back to zero -- the application
-        // is not restarted.
-        primarySwatch: Colors.purple,
+      title: 'E1547',
+      theme: new ThemeData.dark(),
+      home: new Scaffold(
+        appBar: new AppBar(title: const Text("E1547")),
+        body: new GridView.builder(
+          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0, // px
+          ),
+          itemBuilder: (ctx, i) => new Center(child: new Text(i.toString())),
+        ),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful,
-  // meaning that it has a State object (defined below) that contains
-  // fields that affect how it looks.
-
-  // This class is the configuration for the state. It holds the
-  // values (in this case the title) provided by the parent (in this
-  // case the App widget) and used by the build method of the State.
-  // Fields in a Widget subclass are always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  HttpClient _client = new HttpClient();
-  String _imgUrl = "https://blog.vczf.io/img/sketches_144234.jpg";
-  Random _rand = new Random();
-
-  Future _incrementCounter() async {
-    var newImgUrl = await _getPostImgUrl(_rand.nextInt(1196667));
-    setState(() {
-      // This call to setState tells the Flutter framework that
-      // something has changed in this State, which causes it to rerun
-      // the build method below so that the display can reflect the
-      // updated values. If we changed _counter without calling
-      // setState(), then the build method would not be called again,
-      // and so nothing would appear to happen.
-      _imgUrl  = newImgUrl;
-    });
-  }
-
-  Future<String> _getPostImgUrl(int id) async {
-    HttpClientRequest req = await _client.getUrl(Uri.parse(
-          "https://e621.net/post/show.json?id=${id}"));
-    HttpClientResponse response = await req.close();
-
-    var completer = new Completer();
-    var contents = new StringBuffer();
-
-    response.transform(UTF8.decoder).listen((String data) {
-      contents.write(data);
-    }, onDone: () {
-      Map post = JSON.decode(contents.toString());
-      completer.complete(post['file_url']);
-    });
-
-    return completer.future;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance
-    // as done by the _incrementCounter method above.
-    // The Flutter framework has been optimized to make rerunning
-    // build methods fast, so that you can just rebuild anything that
-    // needs updating rather than having to individually change
-    // instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that
-        // was created by the App.build method, and use it to set
-        // our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Image.network(_imgUrl),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
