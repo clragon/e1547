0f6721e4d542a3beccf35db3c9ae0b45d0b06593
perlatus <perlatus@git.email.vczf.io>
2017-04-30 07:34:21 -0400

    Switched to simpler ListView of Posts

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,24 @@ void main() {
   runApp(new E1547App());
 }
 
+// Preview of a post that appears in lists of posts. Mostly just the image.
+class PostPreview extends StatelessWidget {
+  PostPreview(Map this.post, {Key key}) : super(key: key);
+
+  final Map post;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+        child: new Center(
+          child: new Image.network(post['sample_url'],
+            fit: BoxFit.fitWidth)));
+  }
+}
+
 class E1547App extends StatefulWidget {
   @override
   _E1547AppState createState() => new _E1547AppState();
-}
-
-class _E1547GridDelegate extends SliverGridDelegate {
-  SliverGridLayout layout;
-
-  @override
-  SliverGridLayout getLayout(SliverConstraints constraints) {
-    var layout = new SliverGridRegularTileLayout(
-      crossAxisCount: 2,
-      crossAxisStride: 100.0,
-      childCrossAxisExtent: 100.0,
-      mainAxisStride: 100.0,
-      childMainAxisExtent: 100.0,
-    );
-
-    this.layout = layout;
-    return layout;
-  }
-
-  @override
-  bool shouldRelayout(SliverGridDelegate old) => false;
 }
 
 class _E1547AppState extends State<E1547App> {
@@ -46,7 +40,7 @@ class _E1547AppState extends State<E1547App> {
     }
 
     HttpClientResponse response = await _http
-        .getUrl(Uri.parse("https://e621.net/post/index.json?page=1&limit=20"))
+        .getUrl(Uri.parse("https://e621.net/post/index.json?page=1&tags=photonoko&limit=5"))
         .then((HttpClientRequest req) => req.close());
 
     var body = new StringBuffer();
@@ -57,27 +51,13 @@ class _E1547AppState extends State<E1547App> {
   }
 
   Widget _body() {
-    var delegate = new _E1547GridDelegate();
-
-    var grid = new GridView.builder(
-        controller: new ScrollController(),
-        gridDelegate: delegate,
-        itemBuilder: (ctx, i) {
-          return _posts.length > i
-              ? new Center(child: new Text(_posts[i]['id'].toString()))
-              : null;
-        });
-
-    grid.controller.addListener(() {
-      if (delegate.layout != null) {
-        var offset = grid.controller.offset;
-        int first = delegate.layout.getMinChildIndexForScrollOffset(offset);
-        int last = delegate.layout.getMaxChildIndexForScrollOffset(offset);
-        print("visible: $first, $last");
-      }
-    });
-
-    return grid;
+    return new ListView.builder(
+      itemBuilder: (ctx, i) {
+        return _posts.length > i
+            ? new PostPreview(_posts[i])
+            : null;
+      },
+    );
   }
 
   @override
