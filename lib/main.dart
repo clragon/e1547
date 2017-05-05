d11e58bc6051040aeb59eee7507b20fe6e13b7e1
perlatus <perlatus@git.email.vczf.io>
2017-05-04 22:07:52 -0400

    Add a hack for offline detection and refresh

--- lib/main.dart
+++ lib/main.dart
@@ -36,15 +36,31 @@ class _E1547AppState extends State<E1547App> {
 
   List<Map> _posts = [];
 
+  bool _offline = false;
+
   Future<Null> _loadPosts() async {
     if (!_posts.isEmpty) {
       return;
     }
 
+    if (_offline) {
+      return;
+    }
+
+    // TODO: detect network failures => offline
     HttpClientResponse response = await _http
         .getUrl(Uri.parse(
             "https://e621.net/post/index.json?page=1&tags=photonoko&limit=5"))
-        .then((HttpClientRequest req) => req.close());
+        .then((HttpClientRequest req) => req.close(), onError: (e) {
+      print("error with request: $e");
+    });
+
+    setState(() {
+      _offline = response == null;
+    });
+    if (_offline) {
+      return;
+    }
 
     var body = new StringBuffer();
     await response.transform(UTF8.decoder).forEach((s) => body.write(s));
@@ -72,6 +88,23 @@ class _E1547AppState extends State<E1547App> {
     return index;
   }
 
+  AppBar _buildAppBar() {
+    List<Widget> widgets = [];
+    if (_offline) {
+      widgets.add(new IconButton(
+        icon: new Icon(Icons.cloud_off),
+        tooltip: "Reconnect",
+        onPressed: () {
+          print("pressed the cloud_off icon");
+          _offline = false;
+          _loadPosts();
+        },
+      ));
+    }
+
+    return new AppBar(title: new Text("e1547"), actions: widgets);
+  }
+
   @override
   Widget build(BuildContext context) {
     _loadPosts();
@@ -79,7 +112,7 @@ class _E1547AppState extends State<E1547App> {
         title: 'e1547',
         theme: new ThemeData.dark(),
         home: new Scaffold(
-          appBar: new AppBar(title: new Text("e1547")),
+          appBar: _buildAppBar(),
           body: _body(),
           drawer: new Drawer(
               child: new ListView(children: [
@@ -100,7 +133,9 @@ class _SearchFab extends StatelessWidget {
     return new FloatingActionButton(
         onPressed: () {
           print("pressed FAB");
-          Scaffold.of(context).showBottomSheet((context) => new Text("bottom sheet?"));
+          Scaffold
+              .of(context)
+              .showBottomSheet((context) => new Text("bottom sheet?"));
         },
         child: new Icon(Icons.search));
   }
