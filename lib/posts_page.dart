b711a32067cc176f5ac1a8c3c1040be1354440f3
perlatus <perlatus@git.email.vczf.io>
2017-06-21 16:42:40 -0400

    Jump to top of list on refresh and search

--- lib/posts_page.dart
+++ lib/posts_page.dart
@@ -40,6 +40,8 @@ class PostsPage extends StatefulWidget {
 class _PostsPageState extends State<PostsPage> {
   final Logger _log = new Logger('PostsPage');
 
+  final ScrollController _controller = new ScrollController();
+
   Tagset _tags; // Tags used for searching for posts.
   List<Post> _posts = []; // Posts being displayed.
   int _page = _STARTING_PAGE;
@@ -79,6 +81,7 @@ class _PostsPageState extends State<PostsPage> {
     persistence.setTags(_tags);
     _page = _STARTING_PAGE;
     _posts.clear();
+    _controller.jumpTo(0.0);
     _loadNextPage();
   }
 
@@ -184,6 +187,7 @@ class _PostsPageState extends State<PostsPage> {
     }
 
     return new GridView.custom(
+      controller: _controller,
       gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 150.0,
         childAspectRatio: 3 / 5,
