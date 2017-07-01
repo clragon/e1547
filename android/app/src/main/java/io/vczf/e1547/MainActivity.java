9a308dd265658c286fbf687ec003bb99fefc81fc
perlatus <perlatus@git.email.vczf.io>
2017-07-01 18:32:16 -0400

    Add stubs for opening e621/e926 links in app

--- android/app/src/main/java/io/vczf/e1547/MainActivity.java
+++ android/app/src/main/java/io/vczf/e1547/MainActivity.java
@@ -1,14 +1,24 @@
 package io.vczf.e1547;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String TAG = MainActivity.class.getSimpleName();
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    Intent startingIntent = getIntent();
+    if (Intent.ACTION_VIEW.equals(startingIntent.getAction())) {
+      Uri uri = startingIntent.getData();
+      Log.d(TAG, uri.toString());
+    }
   }
 }
