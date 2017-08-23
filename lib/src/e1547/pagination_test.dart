e06b69069adaa3a01f88625d88c28c0f0b88ea11
perlatus <perlatus@git.email.vczf.io>
2017-08-23 15:59:04 -0400

    Write in-order, out-of-bounds, and sparse loading tests

--- lib/src/e1547/pagination_test.dart
+++ lib/src/e1547/pagination_test.dart
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import 'dart:async' show Future;
+
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
@@ -29,15 +31,56 @@ void main() {
     }
   });
 
-  test('Load page 0 and 2', () async {
-    Pagination<String> p = new Pagination<String>(3, (i) async {
-      return new List.filled(3, i.toString());
-    });
+  const numberLoaderMap = const {
+    0: const ['0', '0', '0'],
+    1: const ['1', '1', '1'],
+    2: const ['2', '2', '2'],
+    3: const ['3', '3', '3'],
+    4: const ['4', '4'],
+  };
+
+  Future<List<String>> numberLoader(int page) async {
+    if (page < 0 || page > 4) {
+      return [];
+    } else {
+      return numberLoaderMap[page];
+    }
+  }
+
+  test('Load in order', () async {
+    Pagination<String> p = new Pagination<String>(3, numberLoader);
+    List<String> elementsAns = [];
+    for (int i = 0; i < 4; i++) {
+      expect(await p.loadPage(i), equals(numberLoaderMap[i].length));
+      expect(p.elements, equals(elementsAns..addAll(numberLoaderMap[i])));
+    }
+  });
+
+  test('Load out of bounds', () async {
+    Pagination<String> p = new Pagination<String>(3, numberLoader);
+    expect(await p.loadPage(5), equals(0));
+    expect(p.elements.isEmpty, isTrue);
+    expect(await p.loadPage(-1), equals(0));
+    expect(p.elements.isEmpty, isTrue);
+  });
+
+  test('Load sparse', () async {
+    Pagination<String> p = new Pagination<String>(3, numberLoader);
+    List<String> elementsAns = new List.filled(14, null);
+
+    expect(await p.loadPage(4), equals(2));
+    elementsAns.setRange(12, 14, numberLoaderMap[4]);
+
+    expect(p.elements, equals(elementsAns));
+
+    expect(await p.loadPage(2), equals(3));
+    elementsAns.setRange(6, 9, numberLoaderMap[2]);
+
+    expect(p.elements, equals(elementsAns));
 
     expect(await p.loadPage(0), equals(3));
-    expect(p.elements, equals(['0', '0', '0']));
-    expect(await p.loadPage(2), equals(3));
-    expect(
-        p.elements, equals(['0', '0', '0', null, null, null, '2', '2', '2']));
+    elementsAns.setRange(0, 3, numberLoaderMap[0]);
+
+    expect(p.elements, equals(elementsAns));
   });
 }
