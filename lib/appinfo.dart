c7500f21e97004ad711a81ec742fe4b529810cc1
clragon <kenta3ds.habbo@gmail.com>
2020-03-16 02:23:23 +0100

    added missing file

---
+++ lib/appinfo.dart
@@ -0,0 +1,5 @@
+const String appName = 'e1547';
+const String appVersion = '1.0.0';
+const String defaultEndpoint = 'e926.net';
+const String about = 'An app for e621 and e926.';
+const String developer = 'binaryfloof';
