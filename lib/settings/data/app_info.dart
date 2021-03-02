69a1248b5b9d2e7adbb199b88ac2e1f6c90f41df
clragon <kenta3ds.habbo@gmail.com>
2021-03-02 14:20:26 +0100

    increased version

--- lib/settings/data/app_info.dart
+++ lib/settings/data/app_info.dart
@@ -1,5 +1,5 @@
 const String appName = 'e1547';
-const String appVersion = '1.7.2';
+const String appVersion = '1.7.3';
 const String about = 'An app for e621 and e926.';
 const String developer = 'binaryfloof';
 const String github = 'clragon/e1547';
