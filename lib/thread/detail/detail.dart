29341f1c3eaf4f9d190768b7620f5cc296650d11
clragon <kenta3ds.habbo@gmail.com>
2021-04-15 14:32:13 +0200

    cleaned up code

--- lib/thread/detail/detail.dart
+++ lib/thread/detail/detail.dart
@@ -29,59 +29,6 @@ class _ThreadDetailState extends State<ThreadDetail> {
   }
 
   Widget replyWidget(Reply reply) {
-    String getAge(String date) {
-      Duration duration =
-          DateTime.now().difference(DateTime.parse(date).toLocal());
-
-      List<int> periods = [
-        1,
-        60,
-        3600,
-        86400,
-        604800,
-        2419200,
-        29030400,
-      ];
-
-      int ago;
-      String measurement;
-      for (int period = 0; period <= periods.length; period++) {
-        if (period == periods.length || duration.inSeconds < periods[period]) {
-          if (period != 0) {
-            ago = (duration.inSeconds / periods[period - 1]).round();
-          } else {
-            ago = duration.inSeconds;
-          }
-          bool single = (ago == 1);
-          switch (periods[period - 1] ?? 1) {
-            case 1:
-              measurement = single ? 'second' : 'seconds';
-              break;
-            case 60:
-              measurement = single ? 'minute' : 'minutes';
-              break;
-            case 3600:
-              measurement = single ? 'hour' : 'hours';
-              break;
-            case 86400:
-              measurement = single ? 'day' : 'days';
-              break;
-            case 604800:
-              measurement = single ? 'week' : 'weeks';
-              break;
-            case 2419200:
-              measurement = single ? 'month' : 'months';
-              break;
-            case 29030400:
-              measurement = single ? 'year' : 'years';
-              break;
-          }
-          break;
-        }
-      }
-      return '$ago $measurement ago';
-    }
-
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 8),
       child: Column(
@@ -240,8 +187,8 @@ class _ThreadDetailState extends State<ThreadDetail> {
       ),
       isLoading: provider.pages.value.isEmpty,
       isEmpty: provider.items.isEmpty,
-      onLoading: Text('Loading thread'),
-      onEmpty: Text('Failed to load!'),
+      onLoading: Text('Loading replies'),
+      onEmpty: Text('No replies'),
       floatingActionButton: FutureBuilder(
         future: client.hasLogin,
         builder: (context, snapshot) {
