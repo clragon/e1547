4bd25da4006ee6189ea9f737123416a058452dc3
clragon <kenta3ds.habbo@gmail.com>
2021-06-14 18:04:48 +0200

    moved code to appropriate places

--- lib/follow/pages/tile.dart
+++ lib/follow/pages/tile.dart
@@ -180,3 +180,167 @@ class _FollowTileState extends State<FollowTile> {
     );
   }
 }
+
+class FollowListTile extends StatefulWidget {
+  final Function onEdit;
+  final Function onDelete;
+  final Function onRename;
+  final Follow follow;
+
+  FollowListTile({
+    @required this.follow,
+    @required this.onEdit,
+    @required this.onDelete,
+    @required this.onRename,
+  }) : super(key: ObjectKey(follow));
+
+  @override
+  _FollowListTileState createState() => _FollowListTileState();
+}
+
+class _FollowListTileState extends State<FollowListTile> {
+  String thumbnail;
+
+  Future<void> update() async {
+    thumbnail = await widget.follow.thumbnail;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    db.host.addListener(update);
+    update();
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    db.host.removeListener(update);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget cardWidget(String tag) {
+      return FakeCard(
+        child: TagGesture(
+          tag: tag,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                child: Text(tagToTitle(tag)),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget contextMenu() {
+      return PopupMenuButton<String>(
+        icon: ShadowIcon(
+          Icons.more_vert,
+        ),
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          if (widget.follow.tags.split(' ').length > 1)
+            PopupMenuItem(
+              value: 'rename',
+              child: PopTile(title: 'Rename', icon: Icons.label),
+            ),
+          PopupMenuItem(
+            value: 'edit',
+            child: PopTile(title: 'Edit', icon: Icons.edit),
+          ),
+          PopupMenuItem(
+            value: 'delete',
+            child: PopTile(title: 'Delete', icon: Icons.delete),
+          ),
+        ],
+        onSelected: (value) async {
+          switch (value) {
+            case 'rename':
+              widget.onRename();
+              break;
+            case 'edit':
+              widget.onEdit();
+              break;
+            case 'delete':
+              widget.onDelete();
+              break;
+          }
+        },
+      );
+    }
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned.fill(
+                child: thumbnail != null
+                    ? Opacity(
+                        opacity: 0.8,
+                        child: CachedNetworkImage(
+                          imageUrl: thumbnail,
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : SizedBox.shrink(),
+              ),
+              Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          SearchPage(tags: widget.follow.tags))),
+                  onLongPress: () => wikiSheet(
+                      context: context, tag: tagToName(widget.follow.tags)),
+                  child: ListTile(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            widget.follow.title,
+                            style: thumbnail != null
+                                ? TextStyle(shadows: getTextShadows())
+                                : null,
+                          ),
+                        ),
+                      ],
+                    ),
+                    subtitle: (widget.follow.tags.split(' ').length > 1)
+                        ? Row(children: [
+                            Expanded(
+                              child: Wrap(
+                                direction: Axis.horizontal,
+                                children: widget.follow.tags
+                                    .split(' ')
+                                    .map((tag) => cardWidget(tag))
+                                    .toList(),
+                              ),
+                            ),
+                          ])
+                        : null,
+                    trailing: contextMenu(),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Divider()
+        ],
+      ),
+    );
+  }
+}
