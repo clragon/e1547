a716e25d4d86e7e74c1ec98d0f8d01dc28702bd2
clragon <kenta3ds.habbo@gmail.com>
2021-06-15 13:55:37 +0200

    cleaned up imports

--- lib/interface/pages/tiles.dart
+++ lib/interface/pages/tiles.dart
@@ -1,5 +1,5 @@
 import 'package:e1547/settings.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 mixin TileSizeMixin<T extends StatefulWidget> on State<T> {
   int tileSize;
--- lib/post/pages/detail/image.dart
+++ lib/post/pages/detail/image.dart
@@ -3,7 +3,6 @@ import 'package:e1547/client.dart';
 import 'package:e1547/interface.dart';
 import 'package:e1547/post.dart';
 import 'package:e1547/settings.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:video_player/video_player.dart';
