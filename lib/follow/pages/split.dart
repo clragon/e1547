ba37ba6abeecc246a56632fa9f262b88cde61bd8
clragon <kenta3ds.habbo@gmail.com>
2021-06-15 18:06:32 +0200

    improved refresh indicator

--- lib/follow/pages/split.dart
+++ lib/follow/pages/split.dart
@@ -26,47 +26,47 @@ class _FollowsSplitPageState extends State<FollowsSplitPage>
     }
   }
 
-  Future<void> updateProgress() async {
-    progress = followUpdater.progress.value;
-    update();
-  }
-
   Future<void> refreshFollows({bool force = false}) async {
     await followUpdater.run(force: force);
-    await followUpdater.finish;
     update();
   }
 
-  double notZero(double value) => value < 1 ? 1 : value;
-  int roundedNotZero(double value) => value.round() == 0 ? 1 : value.round();
-
-  int get crossAxisCount {
-    return notZero(MediaQuery.of(context).size.width / tileSize).round();
-  }
-
-  Future<void> initialLoad() async {
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      refreshController
-          .requestRefresh(
-              needCallback: false, duration: Duration(milliseconds: 100))
-          .then((_) async {
-        await refreshFollows();
+  Future<void> updateRefresh() async {
+    progress = followUpdater.progress.value;
+    update();
+    if (refreshController.headerMode.value == RefreshStatus.idle) {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await refreshController.requestRefresh(
+          needCallback: false,
+          duration: Duration(milliseconds: 100),
+        );
+        await followUpdater.finish;
         refreshController.refreshCompleted();
       });
-    });
+    }
+  }
+
+  @override
+  Future<void> afterFollowInit() async {
+    await refreshFollows();
   }
 
   @override
   void initState() {
     super.initState();
-    followUpdater.progress.addListener(updateProgress);
-    initialLoad();
+    followUpdater.addListener(updateRefresh);
   }
 
   @override
   void dispose() {
     super.dispose();
-    followUpdater.progress.removeListener(updateProgress);
+    followUpdater.removeListener(updateRefresh);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    refreshController = RefreshController();
   }
 
   Widget itemBuilder(BuildContext context, int item) {
@@ -74,79 +74,80 @@ class _FollowsSplitPageState extends State<FollowsSplitPage>
   }
 
   StaggeredTile tileBuilder(int item) {
-    double extra = 0.2;
-    return StaggeredTile.count(1, 1 + extra);
+    return StaggeredTile.count(1, 1 * tileHeightFactor);
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget body() {
-      if (!loading) {
-        return StaggeredGridView.countBuilder(
-          key: Key('grid_${[crossAxisCount, safe].join('_')}_key'),
-          crossAxisCount: crossAxisCount,
-          itemCount: follows.length,
-          itemBuilder: itemBuilder,
-          staggeredTileBuilder: tileBuilder,
-          physics: BouncingScrollPhysics(),
-        );
-      } else {
-        return SizedBox.shrink();
+    return LayoutBuilder(builder: (context, constraints) {
+      Widget body() {
+        if (!loading) {
+          return StaggeredGridView.countBuilder(
+            key: Key('grid_${[crossAxisCount, safe].join('_')}_key'),
+            crossAxisCount: crossAxisCount(constraints.maxWidth),
+            itemCount: follows.length,
+            itemBuilder: itemBuilder,
+            staggeredTileBuilder: tileBuilder,
+            physics: BouncingScrollPhysics(),
+          );
+        } else {
+          return SizedBox.shrink();
+        }
       }
-    }
 
-    Widget page() {
-      return PageLoader(
-        onEmpty: Text('No follows'),
-        onLoading: Text('Loading follows'),
-        onError: Text('Failed to load follows'),
-        isError: false,
-        isLoading: loading,
-        isEmpty: follows?.length == 0,
-        child: SmartRefresher(
-          primary: false,
-          scrollController: scrollController,
-          controller: refreshController,
-          header: ClassicHeader(
-            refreshingText:
-                'Refreshing $progress / ${follows?.length ?? '?'}...',
-            completeText: 'Refreshed follows!',
-          ),
-          onRefresh: () async {
-            try {
-              await refreshFollows(force: true);
-              refreshController.refreshCompleted();
-            } on DioError {
-              refreshController.refreshFailed();
-            }
-          },
-          physics: BouncingScrollPhysics(),
-          child: body(),
-        ),
-      );
-    }
-
-    return Scaffold(
-      drawer: NavigationDrawer(),
-      appBar: ScrollingAppbarFrame(
-        child: AppBar(
-          title: Text('Following'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.view_compact),
-              tooltip: 'Combine',
-              onPressed: () => db.followsSplit.value = Future.value(false),
+      Widget page() {
+        return PageLoader(
+          onEmpty: Text('No follows'),
+          onLoading: Text('Loading follows'),
+          onError: Text('Failed to load follows'),
+          isError: false,
+          isLoading: loading,
+          isEmpty: follows?.length == 0,
+          child: SmartRefresher(
+            primary: false,
+            scrollController: scrollController,
+            controller: refreshController,
+            header: ClassicHeader(
+              refreshingText:
+                  'Refreshing $progress / ${follows?.length ?? '?'}...',
+              completeText: 'Refreshed follows!',
             ),
-            IconButton(
-              icon: Icon(Icons.turned_in),
-              tooltip: 'Settings',
-              onPressed: () => Navigator.pushNamed(context, '/following'),
-            )
-          ],
+            onRefresh: () async {
+              try {
+                await refreshFollows(force: true);
+                refreshController.refreshCompleted();
+              } on DioError {
+                refreshController.refreshFailed();
+              }
+            },
+            physics: BouncingScrollPhysics(),
+            child: body(),
+          ),
+        );
+      }
+
+      return Scaffold(
+        drawer: NavigationDrawer(),
+        appBar: ScrollingAppbarFrame(
+          child: AppBar(
+            title: Text('Following'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.view_compact),
+                tooltip: 'Combine',
+                onPressed: () => db.followsSplit.value = Future.value(false),
+              ),
+              IconButton(
+                icon: Icon(Icons.turned_in),
+                tooltip: 'Settings',
+                onPressed: () => Navigator.pushNamed(context, '/following'),
+              )
+            ],
+          ),
+          controller: scrollController,
         ),
-        controller: scrollController,
-      ),
-      body: page(),
-    );
+        body: page(),
+      );
+    });
   }
 }
