85b8812169499e65dba59d90d4e3ab36c404109a
clragon <kenta3ds.habbo@gmail.com>
2021-11-03 17:07:00 +0100

    fixed snackbars (again)

--- lib/post/widgets/search/widgets/snackbar.dart
+++ lib/post/widgets/search/widgets/snackbar.dart
@@ -16,7 +16,7 @@ Future<void> postDownloadingSnackbar(
           : 'Downloaded ${items.length} posts',
       onProgress: (items, index) => items.length == 1
           ? 'Downloading Post #${items.first.id}'
-          : 'Downloading Post #${items.elementAt(index).id} ($index/${items.length})',
+          : 'Downloading Post #${items.elementAt(index).id} (${index + 1}/${items.length})',
       onFailure: (items, index) =>
           'Failed to download post #${items.elementAt(index).id}',
       onCancel: (items, index) => 'Cancelled download',
@@ -27,4 +27,45 @@ Future<void> postFavoritingSnackbar(
   Set<Post> items,
   bool isLiked,
 ) =>
-    Future.value();
+    loadingSnackbar<Post>(
+      context: context,
+      items: items,
+      timeout: Duration(milliseconds: 300),
+      process: isLiked
+          ? (post) async {
+              if (post.isFavorited) {
+                return post.tryRemoveFav(context);
+              } else {
+                return true;
+              }
+            }
+          : (post) async {
+              if (!post.isFavorited) {
+                return post.tryAddFav(context);
+              } else {
+                return true;
+              }
+            },
+      onDone: isLiked
+          ? (items) => items.length == 1
+              ? 'Unfavorited post #${items.first.id}'
+              : 'Unfavorited ${items.length} posts'
+          : (items) => items.length == 1
+              ? 'Favorited post #${items.first.id}'
+              : 'Favorited ${items.length} posts',
+      onProgress: isLiked
+          ? (items, index) => items.length == 1
+              ? 'Unfavoriting Post #${items.first.id}'
+              : 'Unfavoriting Post #${items.elementAt(index).id} (${index + 1}/${items.length})'
+          : (items, index) => items.length == 1
+              ? 'Favoriting Post #${items.first.id}'
+              : 'Favoriting Post #${items.elementAt(index).id} (${index + 1}/${items.length})',
+      onFailure: isLiked
+          ? (items, index) =>
+              'Failed to unfavorite post #${items.elementAt(index).id}'
+          : (items, index) =>
+              'Failed to favorite post #${items.elementAt(index).id}',
+      onCancel: isLiked
+          ? (items, index) => 'Cancelled unfavoriting'
+          : (items, index) => 'Cancelled favoriting',
+    );
