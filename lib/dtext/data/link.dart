ac82112697edabaa10d2faf349abda181f33c207
clragon <kenta3ds.habbo@gmail.com>
2021-11-16 12:40:53 +0100

    added topic last page button

--- lib/dtext/data/link.dart
+++ lib/dtext/data/link.dart
@@ -26,7 +26,7 @@ Map<RegExp, DTextParser> linkRegexes(BuildContext context) {
   return {
     RegExp(
       linkWrap(
-        r'(?<link>(http(s)?)?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))',
+        r'(?<link>(http(s)?):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))',
         false,
       ),
     ): (match, result, state) => parseLink(
