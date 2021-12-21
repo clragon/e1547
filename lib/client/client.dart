ae9cd62a56eb14986baef3aad3bad62ff226248b
clragon <kenta3ds.habbo@gmail.com>
2021-12-21 22:10:32 +0100

    improved client

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -719,20 +719,12 @@ class Credentials {
   }
 }
 
-Future<bool> validateCall(Future<void> Function() call) async {
+Future<bool> validateCall(Future<void> Function() call,
+    {bool allowRedirect = false}) async {
   try {
     await call();
     return true;
-  } on DioError {
-    return false;
-  }
-}
-
-Future<bool> validateRawCall(Future<void> Function() call) async {
-  try {
-    await call();
-    return true;
-  } on DioError catch (e) {
-    return e.response?.statusCode == 302;
+  } on DioError catch (error) {
+    return allowRedirect && error.response?.statusCode == 302;
   }
 }
