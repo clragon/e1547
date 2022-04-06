ea34bb7e40e678c5dcb3187428b060d1efabf383
clragon <kenta3ds.habbo@gmail.com>
2022-04-06 23:27:49 +0200

    corrected imports

--- lib/dtext/data/tags.dart
+++ lib/dtext/data/tags.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TextState {
   bool bold;
--- lib/dtext/widgets/username.dart
+++ lib/dtext/widgets/username.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:username_generator/username_generator.dart';
 
 class UsernameGeneratorData extends InheritedWidget {
