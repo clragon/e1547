c4292b74b4e346dbea47e1f5396cf7c1ef73c355
clragon <kenta3ds.habbo@gmail.com>
2022-07-19 10:39:25 +0200

    fixed history and filter

--- lib/history/widgets/history.dart
+++ lib/history/widgets/history.dart
@@ -1,5 +1,6 @@
 import 'package:async_builder/async_builder.dart';
 import 'package:async_builder/init_builder.dart';
+import 'package:collection/collection.dart';
 import 'package:e1547/history/history.dart';
 import 'package:e1547/history/widgets/appbar.dart';
 import 'package:e1547/interface/interface.dart';
@@ -21,169 +22,174 @@ class _HistoryPageState extends State<HistoryPage> {
   Set<HistoryFilter> filters = HistoryFilter.values.toSet();
 
   String _buildFilter() =>
-      r'^' + filters.map((e) => '(${e.regex})').join('|') + r'$';
+      r'^' '${filters.map((e) => '(${e.regex})').join('|')}' r'$';
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<HistoriesService>(
-      builder: (context, service, child) =>
-          SubValueBuilder<Stream<List<History>>>(
-        create: (context) =>
-            service.watchAll(linkRegex: _buildFilter(), day: search),
-        selector: (context) => [service, _buildFilter(), search],
-        builder: (context, stream) => AsyncBuilder<List<History>>(
-          stream: stream,
-          builder: (context, histories) => SelectionLayout<History>(
-            items: histories,
-            child: Scaffold(
-              appBar: HistorySelectionAppBar(
-                child: DefaultAppBar(
-                  leading: const BackButton(),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('History'),
-                      CrossFade.builder(
-                        showChild: search != null,
-                        builder: (context) => Text(
-                          dateOrName(search!),
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2!
-                              .copyWith(
-                                color:
-                                    Theme.of(context).textTheme.caption!.color,
-                              ),
+    return LimitedWidthLayout(
+      child: Consumer<HistoriesService>(
+        builder: (context, service, child) =>
+            SubValueBuilder<Stream<List<History>>>(
+          create: (context) =>
+              service.watchAll(linkRegex: _buildFilter(), day: search),
+          selector: (context) => [service, _buildFilter(), search],
+          builder: (context, stream) => AsyncBuilder<List<History>>(
+            stream: stream,
+            builder: (context, histories) => SelectionLayout<History>(
+              items: histories,
+              child: Scaffold(
+                appBar: HistorySelectionAppBar(
+                  child: DefaultAppBar(
+                    leading: const BackButton(),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('History'),
+                        CrossFade.builder(
+                          showChild: search != null,
+                          builder: (context) => Text(
+                            dateOrName(search!),
+                            style:
+                                Theme.of(context).textTheme.bodyText2!.copyWith(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .caption!
+                                          .color,
+                                    ),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  actions: const [ContextDrawerButton()],
-                ),
-              ),
-              body: AsyncBuilder<List<History>>(
-                stream: stream,
-                waiting: (context) => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                builder: (context, histories) => histories!.isNotEmpty
-                    ? GroupedListView<History, DateTime>(
-                        padding: defaultActionListPadding
-                            .add(LimitedWidthLayout.of(context).padding),
-                        elements: histories,
-                        order: GroupedListOrder.DESC,
-                        controller: PrimaryScrollController.of(context),
-                        groupBy: (element) =>
-                            DateUtils.dateOnly(element.visitedAt),
-                        groupHeaderBuilder: (element) => SettingsHeader(
-                            title: dateOrName(element.visitedAt)),
-                        itemComparator: (a, b) =>
-                            a.visitedAt.compareTo(b.visitedAt),
-                        itemBuilder: (context, element) =>
-                            HistoryTile(entry: element),
-                      )
-                    : const IconMessage(
-                        icon: Icon(Icons.history),
-                        title: Text('Your history is empty'),
-                      ),
-              ),
-              floatingActionButton: FloatingActionButton(
-                child: const Icon(Icons.search),
-                onPressed: () async {
-                  List<DateTime> dates = await service.dates();
-
-                  DateTime? result = await showDatePicker(
-                    context: context,
-                    initialDate: search ?? DateTime.now(),
-                    firstDate: dates.first,
-                    lastDate: dates.last,
-                    locale: Localizations.localeOf(context),
-                    initialEntryMode: DatePickerEntryMode.calendarOnly,
-                    selectableDayPredicate: (value) =>
-                        dates.any((e) => DateUtils.isSameDay(value, e)),
-                  );
-
-                  ScrollController? scrollController =
-                      PrimaryScrollController.of(context);
-                  if (result != search &&
-                      (scrollController?.hasClients ?? false)) {
-                    scrollController!.animateTo(0,
-                        duration: defaultAnimationDuration,
-                        curve: Curves.easeInOut);
-                  }
-
-                  setState(() {
-                    search = result;
-                  });
-                },
-              ),
-              endDrawer: ContextDrawer(
-                title: const Text('History'),
-                children: [
-                  ListTile(
-                    leading: Icon(
-                      Icons.info_outline,
-                      color: dimTextColor(context),
+                      ],
                     ),
-                    // TODO: build setting for this
-                    subtitle: Text(
-                      'History entries are deleted when they are '
-                      'older than 30 days or '
-                      'there are more then ${NumberFormat.compact().format(3000)} entries.',
-                      style: TextStyle(
+                    actions: const [ContextDrawerButton()],
+                  ),
+                ),
+                body: AsyncBuilder<List<History>>(
+                  stream: stream,
+                  waiting: (context) => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  builder: (context, histories) => histories!.isNotEmpty
+                      ? GroupedListView<History, DateTime>(
+                          padding: defaultActionListPadding
+                              .add(LimitedWidthLayout.of(context).padding),
+                          elements: histories,
+                          order: GroupedListOrder.DESC,
+                          controller: PrimaryScrollController.of(context),
+                          groupBy: (element) =>
+                              DateUtils.dateOnly(element.visitedAt),
+                          groupHeaderBuilder: (element) => SettingsHeader(
+                              title: dateOrName(element.visitedAt)),
+                          itemComparator: (a, b) =>
+                              a.visitedAt.compareTo(b.visitedAt),
+                          itemBuilder: (context, element) =>
+                              HistoryTile(entry: element),
+                        )
+                      : const IconMessage(
+                          icon: Icon(Icons.history),
+                          title: Text('Your history is empty'),
+                        ),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  child: const Icon(Icons.search),
+                  onPressed: () async {
+                    List<DateTime> dates = await service.dates();
+
+                    DateTime? result = await showDatePicker(
+                      context: context,
+                      initialDate: search ?? DateTime.now(),
+                      firstDate: dates.first,
+                      lastDate: dates.last,
+                      locale: Localizations.localeOf(context),
+                      initialEntryMode: DatePickerEntryMode.calendarOnly,
+                      selectableDayPredicate: (value) =>
+                          dates.any((e) => DateUtils.isSameDay(value, e)),
+                    );
+
+                    ScrollController? scrollController =
+                        PrimaryScrollController.of(context);
+                    if (result != search &&
+                        (scrollController?.hasClients ?? false)) {
+                      scrollController!.animateTo(0,
+                          duration: defaultAnimationDuration,
+                          curve: Curves.easeInOut);
+                    }
+
+                    setState(() {
+                      search = result;
+                    });
+                  },
+                ),
+                endDrawer: ContextDrawer(
+                  title: const Text('History'),
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.info_outline,
                         color: dimTextColor(context),
                       ),
-                    ),
-                  ),
-                  const Divider(),
-                  ListTile(
-                    leading: const Icon(Icons.filter_alt),
-                    title: const Text('Filter'),
-                    subtitle: histories != null
-                        ? Text('${histories.length} entries shown')
-                        : null,
-                  ),
-                  const Divider(),
-                  for (final filter in HistoryFilter.values)
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: CheckboxListTile(
-                        title: Text(filter.name),
-                        value: filters.contains(filter),
-                        onChanged: (value) {
-                          if (value == null) {
-                            return;
-                          }
-                          setState(() {
-                            if (value) {
-                              filters.add(filter);
-                            } else {
-                              filters.remove(filter);
-                            }
-                          });
-                        },
+                      // TODO: build setting for this
+                      subtitle: Text(
+                        'History entries are deleted when they are '
+                        'older than 30 days or '
+                        'there are more then ${NumberFormat.compact().format(3000)} entries.',
+                        style: TextStyle(
+                          color: dimTextColor(context),
+                        ),
                       ),
                     ),
-                  const Divider(),
-                  if (histories != null)
-                    InitBuilder<Stream<int>>(
-                      getter: service.watchLength,
-                      builder: (context, stream) => AsyncBuilder<int>(
-                        stream: stream,
-                        builder: (context, value) => CrossFade(
-                          showChild: value != null,
-                          child: Center(
-                            child: Text(
-                              'of ${value ?? 0} entries',
-                              style: TextStyle(
-                                color: dimTextColor(context),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.filter_alt),
+                      title: const Text('Filter'),
+                      subtitle: TweenAnimationBuilder(
+                        tween: IntTween(begin: 0, end: histories?.length ?? 0),
+                        duration: const Duration(milliseconds: 200),
+                        builder: (context, value, child) =>
+                            Text('$value entries shown'),
+                      ),
+                    ),
+                    const Divider(),
+                    for (final filter in HistoryFilter.values)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: CheckboxListTile(
+                          title: Text(filter.title),
+                          value: filters.contains(filter),
+                          onChanged: (value) {
+                            if (value == null) {
+                              return;
+                            }
+                            setState(() {
+                              if (value) {
+                                filters.add(filter);
+                              } else {
+                                filters.remove(filter);
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    const Divider(),
+                    if (histories != null)
+                      InitBuilder<Stream<int>>(
+                        getter: service.watchLength,
+                        builder: (context, stream) => AsyncBuilder<int>(
+                          stream: stream,
+                          builder: (context, value) => CrossFade(
+                            showChild: value != null,
+                            child: Center(
+                              child: Text(
+                                'of ${value ?? 0} entries',
+                                style: TextStyle(
+                                  color: dimTextColor(context),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -198,6 +204,17 @@ enum HistoryFilter {
   pools,
   other;
 
+  String get title {
+    switch (this) {
+      case posts:
+        return 'Posts';
+      case pools:
+        return 'Pools';
+      case other:
+        return 'Other';
+    }
+  }
+
   String? get regex {
     switch (this) {
       case posts:
@@ -205,7 +222,9 @@ enum HistoryFilter {
       case pools:
         return r'/pools/\d+';
       case other:
-        return '.*';
+        return r'^(?:(?!'
+            '(${values.whereNot((e) => e == other).map((e) => e.regex).join('|')})'
+            r').)+$';
     }
   }
 }
