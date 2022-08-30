60b44e1905675a1442594ccefe2c50d581f2ecad
clragon <kenta3ds.habbo@gmail.com>
2022-08-30 21:37:56 +0200

    updated error logging

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,26 @@ import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   Talker talker = Talker();
-  await runZonedGuarded<Future<void>>(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
-      FlutterError.onError =
-          (details) => talker.handle(details.exception, details.stack);
-      await initializeSql();
-      AppInfo appInfo = await initializeAppInfo();
-      Settings settings = await initializeSettings();
-      WindowManager? windowManager = await initializeWindowManager();
-      runApp(
-        MultiProvider(
-          providers: [
-            if (windowManager != null) Provider.value(value: windowManager),
-            Provider.value(value: appInfo),
-            Provider.value(value: settings),
-            Provider.value(value: talker),
-          ],
-          child: const App(),
-        ),
-      );
-    },
-    (error, stack) => talker.handle(error, stack),
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding.instance.platformDispatcher.onError = (error, stack) {
+    talker.handle(error, stack);
+    return false;
+  };
+  FlutterError.onError =
+      (details) => talker.handle(details.exception, details.stack);
+  await initializeSql();
+  AppInfo appInfo = await initializeAppInfo();
+  Settings settings = await initializeSettings();
+  WindowManager? windowManager = await initializeWindowManager();
+  runApp(
+    MultiProvider(
+      providers: [
+        if (windowManager != null) Provider.value(value: windowManager),
+        Provider.value(value: appInfo),
+        Provider.value(value: settings),
+        Provider.value(value: talker),
+      ],
+      child: const App(),
+    ),
   );
 }
