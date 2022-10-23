ac1a21f03b9ae20a7647e5b6e0a39cf34f49cc88
dragon <kenta3ds.habbo@gmail.com>
2022-10-23 17:29:27 +0200

    removed pool thumbnails

--- lib/pool/widgets/pools.dart
+++ lib/pool/widgets/pools.dart
@@ -25,61 +25,58 @@ class _PoolsPageState extends State<PoolsPage> with DrawerEntry {
   Widget build(BuildContext context) {
     return PoolsProvider(
       search: widget.search,
-      child: PoolThumbnailProvider(
-        child: Consumer<PoolsController>(
-          builder: (context, controller, child) => ListenableListener(
-            listener: () async {
-              await controller.waitForFirstPage();
-              await context.read<HistoriesService>().addPoolSearch(
-                    context.read<Client>().host,
-                    controller.search.value,
-                    pools: controller.itemList,
-                  );
-            },
-            listenable: controller.search,
-            child: RefreshableControllerPage.builder(
-              appBar: const DefaultAppBar(
-                title: Text('Pools'),
+      child: Consumer<PoolsController>(
+        builder: (context, controller, child) => ListenableListener(
+          listener: () async {
+            await controller.waitForFirstPage();
+            await context.read<HistoriesService>().addPoolSearch(
+                  context.read<Client>().host,
+                  controller.search.value,
+                  pools: controller.itemList,
+                );
+          },
+          listenable: controller.search,
+          child: RefreshableControllerPage.builder(
+            appBar: const DefaultAppBar(
+              title: Text('Pools'),
+            ),
+            floatingActionButton: SheetFloatingActionButton(
+              actionIcon: Icons.search,
+              builder: (context, actionController) => PoolSearchInput(
+                controller: controller,
+                actionController: actionController,
               ),
-              floatingActionButton: SheetFloatingActionButton(
-                actionIcon: Icons.search,
-                builder: (context, actionController) => PoolSearchInput(
-                  controller: controller,
-                  actionController: actionController,
-                ),
-              ),
-              drawer: const NavigationDrawer(),
-              controller: controller,
-              builder: (context, child) => TileLayout(
-                tileSize: context.watch<Settings>().tileSize.value,
-                child: child,
-              ),
-              child: (context) => PagedMasonryGridView<int, Pool>.count(
-                primary: true,
-                showNewPageProgressIndicatorAsGridChild: false,
-                showNewPageErrorIndicatorAsGridChild: false,
-                showNoMoreItemsIndicatorAsGridChild: false,
-                padding: defaultListPadding,
+            ),
+            drawer: const NavigationDrawer(),
+            controller: controller,
+            builder: (context, child) => TileLayout(
+              tileSize: context.watch<Settings>().tileSize.value,
+              child: child,
+            ),
+            child: (context) => PagedMasonryGridView<int, Pool>.count(
+              primary: true,
+              showNewPageProgressIndicatorAsGridChild: false,
+              showNewPageErrorIndicatorAsGridChild: false,
+              showNoMoreItemsIndicatorAsGridChild: false,
+              padding: defaultListPadding,
+              pagingController: controller,
+              crossAxisCount:
+                  (TileLayout.of(context).crossAxisCount * 0.5).round(),
+              builderDelegate: defaultPagedChildBuilderDelegate<Pool>(
                 pagingController: controller,
-                crossAxisCount:
-                (TileLayout.of(context).crossAxisCount * 0.5).round(),
-                builderDelegate: defaultPagedChildBuilderDelegate<Pool>(
-                  pagingController: controller,
-                  itemBuilder: (context, item, index) =>
-                      LowResCacheSizeProvider(
-                        size: TileLayout.of(context).tileSize * 4,
-                        child: PoolTile(
-                          pool: item,
-                          onPressed: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => PoolPage(pool: item),
-                            ),
-                          ),
-                        ),
+                itemBuilder: (context, item, index) => LowResCacheSizeProvider(
+                  size: TileLayout.of(context).tileSize * 4,
+                  child: PoolTile(
+                    pool: item,
+                    onPressed: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => PoolPage(pool: item),
                       ),
-                  onEmpty: const Text('No pools'),
-                  onError: const Text('Failed to load pools'),
+                    ),
+                  ),
                 ),
+                onEmpty: const Text('No pools'),
+                onError: const Text('Failed to load pools'),
               ),
             ),
           ),
