4f5ce9d9fef502efbd0fe589a83d0cc2e4862ade
dragon <kenta3ds.habbo@gmail.com>
2023-02-26 22:10:32 +0100

    updated app database configuration

--- lib/app/data/database.dart
+++ lib/app/data/database.dart
@@ -1,6 +1,18 @@
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_db_store/dio_cache_interceptor_db_store.dart';
 import 'package:drift/drift.dart';
+import 'package:e1547/interface/interface.dart';
 import 'package:notified_preferences/notified_preferences.dart';
+import 'package:path_provider/path_provider.dart';
+
+Future<AppDatabases> initializeAppdatabases() async => AppDatabases(
+      preferences: await SharedPreferences.getInstance(),
+      followDb: connectDatabase('follows.sqlite'),
+      historyDb: connectDatabase('history.sqlite'),
+      httpCache: DbCacheStore(
+        databasePath: (await getTemporaryDirectory()).path,
+      ),
+    );
 
 /// Holds various databases for the app.
 class AppDatabases {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-import 'package:dio_cache_interceptor_db_store/dio_cache_interceptor_db_store.dart';
 import 'package:e1547/app/app.dart';
 import 'package:e1547/interface/interface.dart';
 import 'package:flutter/material.dart';
-import 'package:notified_preferences/notified_preferences.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:talker/talker.dart';
 
 Future<void> main() async {
@@ -18,16 +15,7 @@ Future<void> main() async {
         Provider.value(value: appInfo),
         Provider.value(value: await initializeWindowManager()),
         Provider.value(value: talker),
-        Provider.value(
-          value: AppDatabases(
-            preferences: await SharedPreferences.getInstance(),
-            followDb: connectDatabase('follows.sqlite'),
-            historyDb: connectDatabase('history.sqlite'),
-            httpCache: DbCacheStore(
-              databasePath: (await getTemporaryDirectory()).path,
-            ),
-          ),
-        ),
+        Provider.value(value: await initializeAppdatabases()),
         Provider.value(value: await initializeCookiesService(appInfo)),
       ],
       child: const App(),
