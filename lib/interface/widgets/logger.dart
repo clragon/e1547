c64d85325471730ee909fa0f88ce2abbea44ac68
dragon <kenta3ds.habbo@gmail.com>
2023-03-24 20:21:17 +0100

    fixed logging on boot

--- lib/interface/widgets/logger.dart
+++ lib/interface/widgets/logger.dart
@@ -265,62 +265,73 @@ class _LoggerErrorNotifierState extends State<LoggerErrorNotifier> {
     if (messenger == null) return;
     if ([LogLevel.critical, LogLevel.error].contains(event.logLevel)) {
       Color background = event.logLevel.color;
-      messenger.clearSnackBars();
-      messenger.showSnackBar(
-        SnackBar(
-          duration: const Duration(seconds: 3),
-          content: Builder(
-            builder: (context) {
-              TextStyle style = Theme.of(context).textTheme.bodyMedium!;
-              Color textColor = style.color!;
-              double textLuminance = textColor.computeLuminance();
-              double colorDifference =
-                  background.computeLuminance() - textLuminance;
-              if (colorDifference.abs() < 0.2) {
-                if (textLuminance > 0.5) {
-                  textColor = ThemeData(brightness: Brightness.light)
-                      .textTheme
-                      .titleMedium!
-                      .color!;
-                } else {
-                  textColor = ThemeData(brightness: Brightness.dark)
-                      .textTheme
-                      .titleMedium!
-                      .color!;
+      try {
+        messenger.clearSnackBars();
+        messenger.showSnackBar(
+          SnackBar(
+            duration: const Duration(seconds: 3),
+            content: Builder(
+              builder: (context) {
+                TextStyle style = Theme.of(context).textTheme.bodyMedium!;
+                Color textColor = style.color!;
+                double textLuminance = textColor.computeLuminance();
+                double colorDifference =
+                    background.computeLuminance() - textLuminance;
+                if (colorDifference.abs() < 0.2) {
+                  if (textLuminance > 0.5) {
+                    textColor = ThemeData(brightness: Brightness.light)
+                        .textTheme
+                        .titleMedium!
+                        .color!;
+                  } else {
+                    textColor = ThemeData(brightness: Brightness.dark)
+                        .textTheme
+                        .titleMedium!
+                        .color!;
+                  }
                 }
-              }
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'A fatal error has occured!',
-                    style: style.copyWith(
-                      color: textColor,
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'A fatal error has occured!',
+                      style: style.copyWith(
+                        color: textColor,
+                      ),
                     ),
-                  ),
-                  Text(
-                    event.logShort.trim(),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    style: style.copyWith(
-                      color: textColor,
+                    Text(
+                      event.logShort.trim(),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: style.copyWith(
+                        color: textColor,
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
+                  ],
+                );
+              },
+            ),
+            backgroundColor: background,
+            behavior: SnackBarBehavior.floating,
+            action: widget.onOpenLogs != null
+                ? SnackBarAction(
+                    label: 'LOGS',
+                    onPressed: widget.onOpenLogs!,
+                  )
+                : null,
           ),
-          backgroundColor: background,
-          behavior: SnackBarBehavior.floating,
-          action: widget.onOpenLogs != null
-              ? SnackBarAction(
-                  label: 'LOGS',
-                  onPressed: widget.onOpenLogs!,
-                )
-              : null,
-        ),
-      );
+        );
+      }
+      // this is necessary, as there is no way to check whether a [Scaffold] is attached to a [ScaffoldMessenger].
+      // If we do not check and the application has an error on boot, it will get stuck in an endless error loop.
+      // ignore: avoid_catching_errors
+      on Error catch (e) {
+        if (!e.toString().contains(
+            'ScaffoldMessenger.showSnackBar was called, but there are currently no descendant Scaffolds to present to')) {
+          rethrow;
+        }
+      }
     }
   }
 
