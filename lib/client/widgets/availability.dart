4f66fa4121d3d7209bf1227c82180665934f555b
dragon <kenta3ds.habbo@gmail.com>
2023-04-16 03:08:38 +0200

    improved client availability checker

--- lib/client/widgets/availability.dart
+++ lib/client/widgets/availability.dart
@@ -15,24 +15,38 @@ class ClientAvailabilityCheck extends StatelessWidget {
   Future<void> check(BuildContext context) async {
     RouterDrawerController controller = context.read<RouterDrawerController>();
     Client client = context.read<Client>();
+    Talker talker = context.read<Talker>();
     try {
       await client.availability();
-    } on ClientException catch (e) {
+    } on ClientException catch (e, stacktrace) {
       if (CancelToken.isCancel(e)) {
         return;
       }
-      switch (e.response?.statusCode) {
+      int? statusCode = e.response?.statusCode;
+      if (statusCode == null) return;
+      switch (statusCode) {
         case HttpStatus.serviceUnavailable:
           controller.navigator!.push(
             MaterialPageRoute(
-              builder: (context) => const ClientAvailabilityResolver(),
+              builder: (context) =>
+                  const HostUnvailablePage(offerResolve: true),
             ),
           );
-          break;
+          return;
         case HttpStatus.forbidden:
-          break;
+          // This could potentially logout the user.
+          // However, it might be returned during Cloudflare API blockages.
+          // Logout the user, and if theyre already logged out, trigger Resolver?
+          return;
       }
-      context.read<Talker>().handle(e);
+      if (500 <= statusCode && statusCode < 600) {
+        controller.navigator!.push(
+          MaterialPageRoute(
+            builder: (context) => const HostUnvailablePage(),
+          ),
+        );
+      }
+      talker.handle(e, stacktrace);
     }
   }
 
@@ -47,23 +61,78 @@ class ClientAvailabilityCheck extends StatelessWidget {
       );
 }
 
-class ClientAvailabilityResolver extends StatefulWidget {
-  const ClientAvailabilityResolver({super.key});
+class HostUnvailablePage extends StatelessWidget {
+  const HostUnvailablePage({super.key, this.offerResolve = false});
+
+  final bool offerResolve;
 
   @override
-  State<ClientAvailabilityResolver> createState() =>
-      _ClientAvailabilityResolverState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CloseButton(),
+        title: const Text('Host unavailable'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.cloud_off, size: 60),
+              const SizedBox(height: 8),
+              Text(
+                'Host unavailable',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'It appears that ${context.watch<Client>().host} is not available!',
+              ),
+              const SizedBox(height: 16),
+              if (offerResolve && (Platform.isAndroid || Platform.isIOS)) ...[
+                const Text(
+                  'Please resolve the issue in the following browser window. '
+                  '\n\nCloudflare captcha cookies will be saved. ',
+                ),
+                const SizedBox(height: 16),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const CookieCapturePage(),
+                    ),
+                  ),
+                  child: const Text('Resolve'),
+                ),
+              ] else
+                DimSubtree(
+                  child: Text(
+                    '\nPlease wait for ${context.watch<Client>().host} to resolve the situation on their end.',
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _ClientAvailabilityResolverState
-    extends State<ClientAvailabilityResolver> {
+class CookieCapturePage extends StatefulWidget {
+  const CookieCapturePage({super.key, this.title});
+
+  final Widget? title;
+
+  @override
+  State<CookieCapturePage> createState() => _CookieCapturePageState();
+}
+
+class _CookieCapturePageState extends State<CookieCapturePage> {
   late final WebViewController controller = WebViewController()
     ..setUserAgent(context.read<Client>().userAgent)
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..setBackgroundColor(Theme.of(context).colorScheme.background)
     ..loadRequest(Uri.https(context.read<Client>().host));
-
-  bool read = false;
 
   Future<void> setCookies(BuildContext context) async {
     CookiesService cookiesService = context.read<CookiesService>();
@@ -74,65 +143,18 @@ class _ClientAvailabilityResolverState
 
   @override
   Widget build(BuildContext context) {
-    return CrossFade(
-      showChild: read,
-      secondChild: Scaffold(
-        appBar: AppBar(
-          leading: const CloseButton(),
-          title: const Text('Host unavailable'),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(Icons.cloud_off, size: 60),
-                const SizedBox(height: 8),
-                Text(
-                  'Host unavailable',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'It appears that ${context.watch<Client>().host} is not available!',
-                ),
-                const SizedBox(height: 16),
-                if (Platform.isAndroid || Platform.isIOS) ...[
-                  const Text(
-                    'If possible, please resolve the situation in the following browser window. '
-                    '\n\nCloudflare cookies will be saved to resolve Cloudflare issues. ',
-                  ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () => setState(() => read = true),
-                    child: const Text('Resolve'),
-                  ),
-                ] else
-                  DimSubtree(
-                    child: Text(
-                      'Only Android and iOS devices are supported for resolving captchas in browser windows.'
-                      '\nPlease wait for ${context.watch<Client>().host} to resolve the situation on their end.',
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CloseButton(),
+        title: widget.title,
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const CloseButton(),
-          title: const Text('Resolve host issue'),
-        ),
-        body: WebViewWidget(controller: controller),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.check),
-          onPressed: () async {
-            setCookies(context);
-            Navigator.of(context).maybePop();
-          },
-        ),
+      body: WebViewWidget(controller: controller),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.check),
+        onPressed: () async {
+          setCookies(context);
+          Navigator.of(context).maybePop();
+        },
       ),
     );
   }
