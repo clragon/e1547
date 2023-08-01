91c52fa5dfe5e03e2df6b8cedec12c649a5d71b1
dragon <kenta3ds.habbo@gmail.com>
2023-08-01 15:58:27 +0200

    switched gallery to paginated pageview

--- lib/post/widgets/detail/gallery.dart
+++ lib/post/widgets/detail/gallery.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:e1547/interface/interface.dart';
 import 'package:e1547/post/post.dart';
 import 'package:flutter/material.dart';
@@ -26,75 +24,53 @@ class PostDetailGallery extends StatefulWidget {
 }
 
 class _PostDetailGalleryState extends State<PostDetailGallery> {
-  bool hasRequestedNextPage = false;
   late PageController pageController = widget.pageController ??
       PageController(initialPage: widget.initialPage ?? 0);
-
-  void loadNextPage(int index) {
-    if (!hasRequestedNextPage) {
-      int newPageRequestTriggerIndex =
-          max(0, ((widget.controller.items?.length ?? 0) - 3) - 1);
-      if (widget.controller.nextPageKey != null &&
-          index >= newPageRequestTriggerIndex) {
-        WidgetsBinding.instance.addPostFrameCallback((_) => widget
-            .controller.paging
-            .notifyPageRequestListeners(widget.controller.nextPageKey!));
-        hasRequestedNextPage = true;
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: widget.controller,
       child: Consumer<PostsController>(
-        builder: (context, controller, child) => SubListener(
-          listenable: controller,
-          listener: () {
-            if (widget.controller.paging.value.status == PagingStatus.ongoing) {
-              hasRequestedNextPage = false;
-            }
-          },
-          builder: (context) => GalleryButtons(
-            controller: pageController,
-            child: PageView.builder(
-              controller: pageController,
-              itemBuilder: (context, index) {
-                loadNextPage(index);
-                return SubScrollController(
-                  builder: (context, scrollController) =>
-                      PrimaryScrollController(
-                    controller: scrollController,
-                    child: PostDetail(
-                      post: controller.items![index],
-                      onTapImage: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => PostsRouteConnector(
+        builder: (context, controller, child) => GalleryButtons(
+          controller: pageController,
+          child: PagedPageView(
+            pageController: pageController,
+            pagingController: widget.controller.paging,
+            builderDelegate: defaultPagedChildBuilderDelegate<Post>(
+              pagingController: controller.paging,
+              onEmpty: const Text('No posts'),
+              onError: const Text('Failed to load posts'),
+              itemBuilder: (context, item, index) => SubScrollController(
+                builder: (context, scrollController) => PrimaryScrollController(
+                  controller: scrollController,
+                  child: PostDetail(
+                    post: item,
+                    onTapImage: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => PostsRouteConnector(
+                          controller: controller,
+                          child: PostFullscreenGallery(
                             controller: controller,
-                            child: PostFullscreenGallery(
-                              controller: controller,
-                              initialPage: index,
-                              onPageChanged: pageController.jumpToPage,
-                            ),
+                            initialPage: index,
+                            onPageChanged: pageController.jumpToPage,
                           ),
                         ),
                       ),
                     ),
                   ),
-                );
-              },
-              itemCount: controller.items?.length ?? 0,
-              onPageChanged: (index) {
-                widget.onPageChanged?.call(index);
-                preloadPostImages(
-                  context: context,
-                  index: index,
-                  posts: controller.items!,
-                  size: PostImageSize.sample,
-                );
-              },
+                ),
+              ),
             ),
+            onPageChanged: (index) {
+              widget.onPageChanged?.call(index);
+              preloadPostImages(
+                context: context,
+                index: index,
+                posts: controller.items!,
+                size: PostImageSize.sample,
+              );
+            },
           ),
         ),
       ),
