31a4404203305dc80fd879b4cbb4d814533864db
dragon <kenta3ds.habbo@gmail.com>
2023-08-21 04:49:07 +0200

    fixed adding post search history entries

--- lib/history/data/service.dart
+++ lib/history/data/service.dart
@@ -131,7 +131,7 @@ class HistoriesService extends HistoriesDatabase with ChangeNotifier {
           link: Uri(
             path: '/posts',
             queryParameters: {
-              if (search.isNotEmpty) 'tags': QueryMap.parse(search),
+              if (search.isNotEmpty) 'tags': QueryMap.parse(search).toString(),
             },
           ).toString(),
           thumbnails: _getThumbnails(posts),
