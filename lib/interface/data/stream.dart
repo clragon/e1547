e94f3617c54cd7ba8989af37bbc9740430dd6836
dragon <kenta3ds.habbo@gmail.com>
2023-09-04 23:36:36 +0200

    added streamfuture.value

--- lib/interface/data/stream.dart
+++ lib/interface/data/stream.dart
@@ -5,6 +5,9 @@ import 'package:async/async.dart';
 /// This is useful for situations where you usually only need the first item of a
 /// stream, but sometimes need to access the stream itself.
 class StreamFuture<T> extends DelegatingFuture<T> {
+  /// Creates a [StreamFuture] from a [stream].
+  ///
+  /// If the [stream] is not a broadcast stream, it will be converted to one.
   factory StreamFuture(Stream<T> stream) {
     if (stream.isBroadcast) {
       return StreamFuture._(stream);
@@ -13,11 +16,14 @@ class StreamFuture<T> extends DelegatingFuture<T> {
     }
   }
 
+  /// Creates a [StreamFuture] that completes with [value].
+  factory StreamFuture.value(T value) => StreamFuture(Stream<T>.value(value));
+
   StreamFuture._(Stream<T> stream)
       : assert(
-  stream.isBroadcast,
-  'StreamFuture can only be created from a broadcast stream.',
-  ),
+          stream.isBroadcast,
+          'StreamFuture can only be created from a broadcast stream.',
+        ),
         _stream = stream,
         super(stream.first);
 
