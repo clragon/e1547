f5a0a8c973a153ac25d16544f9af1ff4adbbf24e
dragon <kenta3ds.habbo@gmail.com>
2023-09-04 23:19:52 +0200

    improved stream future code

--- lib/interface/data/stream.dart
+++ lib/interface/data/stream.dart
@@ -1,17 +1,32 @@
 import 'package:async/async.dart';
 
+/// A [Future] that wraps a [Stream].
+///
+/// This is useful for situations where you usually only need the first item of a
+/// stream, but sometimes need to access the stream itself.
 class StreamFuture<T> extends DelegatingFuture<T> {
-  StreamFuture(Stream<T> stream)
-      : _stream = stream,
-        super(stream.isBroadcast
-            ? stream.first
-            : stream.asBroadcastStream().first);
+  factory StreamFuture(Stream<T> stream) {
+    if (stream.isBroadcast) {
+      return StreamFuture._(stream);
+    } else {
+      return StreamFuture._(stream.asBroadcastStream());
+    }
+  }
+
+  StreamFuture._(Stream<T> stream)
+      : assert(
+  stream.isBroadcast,
+  'StreamFuture can only be created from a broadcast stream.',
+  ),
+        _stream = stream,
+        super(stream.first);
 
   final Stream<T> _stream;
 
   Stream<T> get stream => _stream;
 }
 
+/// An extension on [Stream] to easily create a [StreamFuture].
 extension StreamFutureExtension<T> on Stream<T> {
   StreamFuture<T> get future => StreamFuture(this);
 }
