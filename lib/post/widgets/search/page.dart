5bcddc6f90b7c1dede339e87e56ff66d7114cdca
clragon <clragon@clynamic.net>
2024-01-21 02:36:01 +0100

    removed unnecessary limited width layout

--- lib/post/widgets/search/page.dart
+++ lib/post/widgets/search/page.dart
@@ -69,8 +69,7 @@ class _PostsPageState extends State<PostsPage> {
             drawer: const RouterDrawer(),
             endDrawer: endDrawer(),
             floatingActionButton: floatingActionButton(),
-            builder: (context, child) =>
-                LimitedWidthLayout(child: TileLayout(child: child)),
+            builder: (context, child) => TileLayout(child: child),
             controller: widget.controller,
             child: (context) => postDisplay(
               context: context,
