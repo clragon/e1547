30c6ce7d66ed7073f5dac6e235eb62a0c587c23a
clragon <clragon@clynamic.net>
2025-08-03 18:01:20 +0200

    improved posts endpoint unwrapping

--- lib/post/data/client.dart
+++ lib/post/data/client.dart
@@ -3,13 +3,7 @@ import 'package:e1547/post/post.dart';
 import 'package:e1547/stream/stream.dart';
 
 class PostClient {
-  PostClient({required this.dio}) {
-    if (!dio.interceptors.any(
-      (interceptor) => interceptor is _PostsJsonUnwrapperInterceptor,
-    )) {
-      dio.interceptors.add(_PostsJsonUnwrapperInterceptor());
-    }
-  }
+  PostClient({required this.dio});
 
   final Dio dio;
   final PagedValueCache<QueryKey, int, Post> cache = PagedValueCache(
@@ -24,6 +18,7 @@ class PostClient {
             id,
             fetch: () => dio
                 .get('/posts/$id.json', cancelToken: cancelToken)
+                .then(_unwrapPostsResponse)
                 .then((response) => E621Post.fromJson(response.data)),
           )
           .future;
@@ -40,6 +35,7 @@ class PostClient {
                   queryParameters: {'page': page, 'limit': limit},
                   cancelToken: cancelToken,
                 )
+                .then(_unwrapPostsResponse)
                 .then(
                   (response) =>
                       response.data.map<Post>(E621Post.fromJson).toList(),
@@ -62,25 +58,21 @@ class PostClient {
   void dispose() {
     cache.dispose();
   }
-}
 
-/// Unfucks the e621 API JSON response for Posts.
-/// For reasons incomprehensible to mere mortals, this singular endpoint
-/// will always wrap the response in an object containing nothing but a single key.
-/// Either `posts` or `post` depending on the endpoint used.
-/// This is completely useless behaviour, and we don't want to deal with it later down the line.
-///
-/// This code is intentionally crude, as we don't want to waste performance on this.
-class _PostsJsonUnwrapperInterceptor extends Interceptor {
-  @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    if (response.requestOptions.path.contains('/posts') &&
-        response.data is Map<String, dynamic>) {
+  /// Unfucks the e621 API JSON response for Posts.
+  /// For reasons incomprehensible to mere mortals, this singular endpoint
+  /// will always wrap the response in an object containing nothing but a single key.
+  /// Either `posts` or `post` depending on the endpoint used.
+  /// This is completely useless behaviour, and we don't want to deal with it later down the line.
+  ///
+  /// This code is intentionally crude, as we don't want to waste performance on this.
+  Response _unwrapPostsResponse(Response response) {
+    if (response.data is Map<String, dynamic>) {
       final inner = response.data['posts'] ?? response.data['post'];
       if (inner != null) {
         response.data = inner;
       }
     }
-    super.onResponse(response, handler);
+    return response;
   }
 }
